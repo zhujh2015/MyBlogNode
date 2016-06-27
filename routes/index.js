ab3b7e544b397508db8062414e7ec9fe45603d54
zhujh2015 <zhujunhua0401@126.com>
2016-06-27 22:15:02 +0800

    0624

--- routes/index.js
+++ routes/index.js
@@ -78,6 +78,16 @@ router.get('/search', function (req, res) {
   });
 });
 
+/*友情链接*/
+
+router.get('/links',function(req,res){
+  res.render('links',{
+    title:'友情链接',
+    user:req.session.user,
+    success:req.flash('success').toString(),
+    error:req.flash('error').toString()
+  });
+});
 
 
 
@@ -235,7 +245,7 @@ router.post('/login', function (req, res)
   router.post('/post', function (req, res) {
     var currentUser = req.session.user,
       tags=[req.body.tag1,req.body.tag2,req.body.tag3],
-      post = new Post(currentUser.name, req.body.title,tags, req.body.post);
+      post = new Post(currentUser.name,currentUser.head, req.body.title,tags, req.body.post);
   post.save(function (err) {
     if (err) {
       req.flash('error', err); 
@@ -377,7 +387,12 @@ router.post('/u/:name/:day/:title',function(req,res){
         time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + 
              date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
 
+ 
+ var md5=crypto.createHash('md5'),
+   email_MD5=md5.update(req.body.email.toLowerCase()).digest('hex'),
+   head="http://www.gravatar.com/avatar/"+email_MD5+"?s=48";
   var comment={name:req.body.name,
+    head:head,
     email:req.body.email,website:req.body.website,
     time:time,comment:req.body.comment};
 
@@ -391,6 +406,33 @@ router.post('/u/:name/:day/:title',function(req,res){
       res.redirect('back');
 
     });
+});
+
+/*转载*/
+router.get('/reprint/:name/:day/:title', checkLogin);
+router.get('/reprint/:name/:day/:title', function (req, res) {
+  Post.edit(req.params.name, req.params.day, req.params.title, function (err, post) {
+    if (err) {
+      req.flash('error', err); 
+      return res.redirect(back);
+    }
+    var currentUser = req.session.user,
+        reprint_from = {name: post.name, day: post.time.day, title: post.title},
+        reprint_to = {name: currentUser.name, head: currentUser.head};
+    Post.reprint(reprint_from, reprint_to, function (err, post) {
+      if (err) {
+        req.flash('error', err); 
+        return res.redirect('back');
+      }
+      req.flash('success', '转载成功!');
+      var url = encodeURI('/u/' + post.name + '/' + post.time.day + '/' + post.title);
+      //跳转到转载后的文章页面
+      res.redirect(url);
+    });
+  });
+});
+router.use(function(req,res){
+  res.render("404");
 });
 
   function checkLogin(req,res,next) {
